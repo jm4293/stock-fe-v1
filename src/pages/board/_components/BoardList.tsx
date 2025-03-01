d9eeedd8d7e3ecd1c70bdd2e3db86484fa4799b9
jm4293 <dlwoals4293@gmail.com>
2025-03-02 02:35:04 +0900

    [Refactor]: 무한스크롤 적용

--- src/pages/board/_components/BoardList.tsx
+++ src/pages/board/_components/BoardList.tsx
@@ -1,36 +1,56 @@
+import { useEffect, useRef } from 'react';
 import { useBoardListQuery } from '@/hooks/board';
 import { CommentSvg, HeartSvg } from '@/asset/svg';
 import { Text } from '@/components/text';
 import { useNavigate } from 'react-router-dom';
 
-interface IProps {}
-
 export const BoardList = () => {
   const navigate = useNavigate();
+
+  const observerRef = useRef<IntersectionObserver | null>(null);
+  const loadMoreRef = useRef<HTMLDivElement | null>(null);
 
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } = useBoardListQuery();
 
   const onClickHandler = (params: { event: React.MouseEvent<HTMLDivElement, MouseEvent>; boardSeq: number }) => {
     const { event, boardSeq } = params;
-
     event.stopPropagation();
-
     navigate(`/board/detail/${boardSeq}`);
   };
 
-  console.log('data', data);
+  useEffect(() => {
+    if (observerRef.current) {
+      observerRef.current.disconnect();
+    }
+
+    observerRef.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting && hasNextPage && !isFetchingNextPage) {
+        fetchNextPage();
+      }
+    });
+
+    if (loadMoreRef.current) {
+      observerRef.current.observe(loadMoreRef.current);
+    }
+
+    return () => {
+      if (observerRef.current) {
+        observerRef.current.disconnect();
+      }
+    };
+  }, [hasNextPage, isFetchingNextPage, fetchNextPage]);
 
   return (
-    <div className="pt-2">
+    <>
       {status === 'success' ? (
-        <div className="flex flex-col items-center gap-10">
+        <div className="flex flex-col items-center gap-6">
           {data?.map((page) =>
             page.data.data.boards.map((board) => (
               <div
                 key={board.boardSeq}
-                className="w-full px-6 flex flex-col gap-6"
+                className="w-full flex flex-col gap-3"
                 onClick={(event) => onClickHandler({ event, boardSeq: board.boardSeq })}>
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-1">
                   <Text className="font-bold" value={board.title} color="black" />
                   <Text className="line-clamp-2" value={board.content} color="gray" />
                 </div>
@@ -38,15 +58,16 @@ export const BoardList = () => {
                 <div className="flex justify-between">
                   <div className="flex gap-4">
                     <div className="flex gap-1">
-                      <HeartSvg color="#9470DC" />
+                      <HeartSvg color="#989898" />
                       <Text value="0" color="gray" />
                     </div>
                     <div className="flex gap-1">
-                      <CommentSvg color="#9470DC" />
+                      <CommentSvg color="#989898" />
                       <Text value={String(board.commentTotal)} color="gray" />
                     </div>
                   </div>
-                  <div className="flex gap-2">
+
+                  <div className="flex gap-1">
                     <Text value="조회수" color="black" />
                     <Text value={String(board.viewCount)} color="gray" />
                   </div>
@@ -55,15 +76,13 @@ export const BoardList = () => {
             )),
           )}
 
-          {hasNextPage && (
-            <button onClick={() => fetchNextPage()} disabled={isFetchingNextPage}>
-              {isFetchingNextPage ? '로딩중...' : '더보기'}
-            </button>
-          )}
+          <div ref={loadMoreRef} className="h-10 flex justify-center items-center">
+            {isFetchingNextPage && <div>로딩 중...</div>}
+          </div>
         </div>
       ) : (
         <div>Loading...</div>
       )}
-    </div>
+    </>
   );
 };
