4986efabea95537f30c646b164707c59ec304e44
jm4293 <dlwoals4293@gmail.com>
2025-03-02 23:45:06 +0900

    [Refactor]: 게시판 무한스크롤 코드 수정

--- src/hooks/board/query/useBoardListQuery.tsx
+++ src/hooks/board/query/useBoardListQuery.tsx
@@ -1,12 +1,14 @@
-import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import BoardApi from '@/api-url/board/board.api';
 
 export const useBoardListQuery = () => {
   return useInfiniteQuery({
     queryKey: ['board-list'],
-    queryFn: ({ pageParam = 1 }) => BoardApi.getBoardList(pageParam),
+    queryFn: ({ pageParam }) => BoardApi.getBoardList(pageParam),
     getNextPageParam: (lastPage: any) => {
-      return lastPage.data.data.nextPage;
+      const { nextPage } = lastPage.data.data;
+
+      return nextPage;
     },
     select: (data) => {
       return data.pages;
--- src/pages/board/_components/BoardList.tsx
+++ src/pages/board/_components/BoardList.tsx
@@ -3,6 +3,7 @@ import { useBoardListQuery } from '@/hooks/board';
 import { CommentSvg, HeartSvg } from '@/asset/svg';
 import { Text } from '@/components/text';
 import { useNavigate } from 'react-router-dom';
+import { Loading } from '@/components/loading';
 
 export const BoardList = () => {
   const navigate = useNavigate();
@@ -10,11 +11,12 @@ export const BoardList = () => {
   const observerRef = useRef<IntersectionObserver | null>(null);
   const loadMoreRef = useRef<HTMLDivElement | null>(null);
 
-  const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } = useBoardListQuery();
+  const { data, hasNextPage, fetchNextPage, isFetchingNextPage } = useBoardListQuery();
 
   const onClickHandler = (params: { event: React.MouseEvent<HTMLDivElement, MouseEvent>; boardSeq: number }) => {
     const { event, boardSeq } = params;
     event.stopPropagation();
+
     navigate(`/board/detail/${boardSeq}`);
   };
 
@@ -23,9 +25,9 @@ export const BoardList = () => {
       observerRef.current.disconnect();
     }
 
-    observerRef.current = new IntersectionObserver((entries) => {
+    observerRef.current = new IntersectionObserver(async (entries) => {
       if (entries[0].isIntersecting && hasNextPage && !isFetchingNextPage) {
-        fetchNextPage();
+        await fetchNextPage();
       }
     });
 
@@ -41,48 +43,44 @@ export const BoardList = () => {
   }, [hasNextPage, isFetchingNextPage, fetchNextPage]);
 
   return (
-    <>
-      {status === 'success' ? (
-        <div className="flex flex-col items-center gap-6">
-          {data?.map((page) =>
-            page.data.data.boards.map((board) => (
-              <div
-                key={board.boardSeq}
-                className="w-full flex flex-col gap-3"
-                onClick={(event) => onClickHandler({ event, boardSeq: board.boardSeq })}>
-                <div className="flex flex-col gap-1">
-                  <Text className="font-bold" value={board.title} color="black" />
-                  <Text className="line-clamp-2" value={board.content} color="gray" />
+    <div className="flex flex-col items-center gap-6">
+      {data?.map((page) => {
+        const { boards } = page.data.data;
+
+        return boards.map((board) => (
+          <div
+            key={board.boardSeq}
+            className="w-full flex flex-col gap-3"
+            onClick={(event) => onClickHandler({ event, boardSeq: board.boardSeq })}>
+            <div className="flex flex-col gap-1">
+              <Text className="font-bold" value={board.title} color="black" size="middle" />
+              <Text className="line-clamp-2" value={board.content} color="gray" />
+            </div>
+
+            <div className="flex justify-between">
+              <div className="flex gap-4">
+                <div className="flex gap-1">
+                  <HeartSvg color="#989898" />
+                  <Text value="0" color="gray" />
                 </div>
-
-                <div className="flex justify-between">
-                  <div className="flex gap-4">
-                    <div className="flex gap-1">
-                      <HeartSvg color="#989898" />
-                      <Text value="0" color="gray" />
-                    </div>
-                    <div className="flex gap-1">
-                      <CommentSvg color="#989898" />
-                      <Text value={String(board.commentTotal)} color="gray" />
-                    </div>
-                  </div>
-
-                  <div className="flex gap-1">
-                    <Text value="조회수" color="black" />
-                    <Text value={String(board.viewCount)} color="gray" />
-                  </div>
+                <div className="flex gap-1">
+                  <CommentSvg color="#989898" />
+                  <Text value={String(board.commentTotal)} color="gray" />
                 </div>
               </div>
-            )),
-          )}
 
-          <div ref={loadMoreRef} className="h-10 flex justify-center items-center">
-            {isFetchingNextPage && <div>로딩 중...</div>}
+              <div className="flex gap-1">
+                <Text value="조회수" color="black" />
+                <Text value={String(board.viewCount)} color="gray" />
+              </div>
+            </div>
           </div>
-        </div>
-      ) : (
-        <div>Loading...</div>
-      )}
-    </>
+        ));
+      })}
+
+      <div ref={loadMoreRef} className="h-10 flex justify-center items-center">
+        {isFetchingNextPage && <Loading />}
+      </div>
+    </div>
   );
 };
