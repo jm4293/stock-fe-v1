72d4fd8687966ac6736ed71b1d4991419e188ec1
jm4293 <dlwoals4293@gmail.com>
2025-03-04 23:15:42 +0900

    [Chore]: vite-plugin-static-copy 설정, firebase env 설정

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,26 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
+
+const firebaseConfig = {
+  apiKey: '${VITE_FIREBASE_API_KEY}',
+  authDomain: '${VITE_FIREBASE_AUTH_DOMAIN}',
+  projectId: '${VITE_FIREBASE_PROJECT_ID}',
+  storageBucket: '${VITE_FIREBASE_STORAGE_BUCKET}',
+  messagingSenderId: '${VITE_FIREBASE_MESSAGING_SENDER_ID}',
+  appId: '${VITE_FIREBASE_APP_ID}',
+  measurementId: '${VITE_FIREBASE_MEASUREMENT_ID}',
+};
+
+// Firebase 앱 초기화
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+// 백그라운드 메시지 처리
+messaging.onBackgroundMessage((payload) => {
+  console.log('Received background message:', payload);
+
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: '/firebase-logo.png',
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,40 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
   server: {
     port: 31180,
   },
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'firebase-messaging-sw.js',
+          dest: '',
+          transform: (contents) => {
+            const env: { [key: string]: string | undefined } = {
+              VITE_FIREBASE_API_KEY: process.env.VITE_FIREBASE_API_KEY,
+              VITE_FIREBASE_AUTH_DOMAIN: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+              VITE_FIREBASE_PROJECT_ID: process.env.VITE_FIREBASE_PROJECT_ID,
+              VITE_FIREBASE_STORAGE_BUCKET: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+              VITE_FIREBASE_MESSAGING_SENDER_ID: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+              VITE_FIREBASE_APP_ID: process.env.VITE_FIREBASE_APP_ID,
+              VITE_FIREBASE_MEASUREMENT_ID: process.env.VITE_FIREBASE_MEASUREMENT_ID,
+            };
+
+            let fileContent = contents.toString();
+            Object.keys(env).forEach((key) => {
+              fileContent = fileContent.replace(new RegExp(`\\\${${key}}`, 'g'), env[key] || '');
+            });
+
+            return fileContent;
+          },
+        },
+      ],
+    }),
+  ],
 });
