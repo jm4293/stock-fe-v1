738ea99ded62b1247f41616257b0b22a8fd313d4
jm4293 <dlwoals4293@gmail.com>
2025-03-05 23:12:36 +0900

    [Chore]: vite env 추가

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,6 +1,22 @@
 interface ImportMetaEnv {
   readonly VITE_API_URL: string;
   readonly VITE_API_PORT: string;
+  readonly VITE_API_PREFIX: string;
+
+  readonly VITE_KAKAO_APP_KEY: string;
+
+  readonly VITE_GOOGLE_OAUTH_CLIEND_ID: string;
+  readonly VITE_GOOGLE_OAUTH_REDIRECT_URL: string;
+
+  readonly VITE_FIREBASE_API_KEY: string;
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+  readonly VITE_FIREBASE_PROJECT_ID: string;
+  readonly VITE_FIREBASE_STORAGE_BUCKET: string;
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+  readonly VITE_FIREBASE_APP_ID: string;
+  readonly VITE_FIREBASE_MEASUREMENT_ID: string;
+
+  readonly VITE_FIREBASE_WEB_PUSH_TOKEN: string;
 }
 
 interface ImportMeta {
