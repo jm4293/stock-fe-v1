7ace251612b238d568af4ef74875399c95afabe0
jm4293 <dlwoals4293@gmail.com>
2025-03-10 00:12:26 +0900

    [Chore]: 헤더 텍스트 수정

--- src/common/layout/Header.tsx
+++ src/common/layout/Header.tsx
@@ -2,6 +2,17 @@ import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { Text } from '@/components/text';
 import { BackSvg, BellSvg } from '@/asset/svg';
 
+const headerPathName: { [key: string]: string } = {
+  '/home': '홈',
+  '/stock': '주식',
+  '/board': '게시판',
+  '/board/detail': '게시판 작성',
+  '/mypage': '마이페이지',
+  '/mypage/board': '작성한 게시글',
+  '/mypage/comment': '작성한 댓글',
+  '/notification': '알림',
+};
+
 export const Header = () => {
   const params = useParams();
   const location = useLocation();
@@ -9,50 +20,38 @@ export const Header = () => {
 
   const state = localStorage.getItem('state');
 
-  const pathName = () => {
-    const { pathname } = location;
-
-    const currentPath = pathname.split('/')[1];
-
-    switch (currentPath) {
-      case 'home':
-        return '홈';
-      case 'stock':
-        return '주식';
-      case 'board':
-        return '게시판';
-      case 'mypage':
-        return '마이페이지';
-      case 'notification':
-        return '알림';
-      default:
-        return '홈';
-    }
-  };
-
   const onBackButtonClick = (event: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
     event.stopPropagation();
-
     navigate(-1);
   };
 
   const onBellButtonClick = (event: React.MouseEvent<SVGSVGElement, MouseEvent>) => {
     event.stopPropagation();
-
     if (!state) {
       alert('로그인이 필요한 서비스입니다.');
+
       navigate('/auth/login');
     } else {
       navigate('/notification');
     }
   };
 
+  const getHeaderTitle = (pathname: string) => {
+    if (pathname.startsWith('/board/detail/')) {
+      return '게시판 상세';
+    }
+
+    if (pathname === '/board/detail') {
+      return '게시판 등록';
+    }
+
+    return headerPathName[pathname] || '';
+  };
+
   return (
     <div className="header">
       <BackSvg color="#000000" onClick={(event) => onBackButtonClick(event)} />
-
-      <Text value={pathName()} color="#000000" size="lg" />
-
+      <Text value={getHeaderTitle(location.pathname)} color="#000000" size="lg" />
       <BellSvg color="#989898" onClick={(event) => onBellButtonClick(event)} />
     </div>
   );
