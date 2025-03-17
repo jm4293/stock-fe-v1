aebc1d76788a4366a5fb2b39475debc94ff4aac2
jm4293 <dlwoals4293@gmail.com>
2025-03-17 21:07:19 +0900

    [Chore]:

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,21 +2,7 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        active: {
-          bg: "#9470DC",
-          text: "#FFFFFF",
-        },
-        disabled: {
-          bg: "#F0F0F0",
-          text: "#989898",
-        },
-        input: {
-          text: "#989898",
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
